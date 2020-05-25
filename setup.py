5a2fcceee38c947cda66af4a93a806c6988438a6
Omar Somai <omar.somai@supcom.tn>
2020-05-25 19:50:52 +0200

    structure of project

--- setup.py
+++ setup.py
@@ -1,4 +1,20 @@
-from rectool.d4rm_tool import main
+from setuptools import setup, find_packages
 
-if __name__ == '__main__':
-    main()
+
+with open('README.md') as f:
+    readme = f.read()
+
+with open('LICENSE') as f:
+    license = f.read()
+
+setup(
+    name='rectool',
+    version='0.1.0',
+    description='Video and GPS stream recording tool',
+    long_description=readme,
+    author='Omar Somai',
+    author_email='omar.somai@knorr-bremse.com',
+    url='https://github.com/omarsomey/Digits4RailMaps',
+    license=license,
+    packages=find_packages(exclude=('tests', 'docs'))
+)
